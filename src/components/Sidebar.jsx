7c753ceb7c3f32b8443a01434007877d0d5d68f4
Vitor Hugo Pinto <vhpinto1@gmail.com>
2022-10-05 12:46:23 -0300

    Estilização e término da sidebard

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -6,15 +8,19 @@ export function Sidebar() {
             <img 
                 className={styles.cover}
                 src='https://images.unsplash.com/photo-1661956602868-6ae368943878?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=40'
-            >
-            </img>
+            />
             <div className={styles.profile}>
+                <img
+                    className={styles.avatar}
+                    src='https://github.com/vhpinto.png'
+                />
                 <strong>Vitor Hugo</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href='#'>
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
