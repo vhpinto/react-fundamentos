b322fb3fad28d0259d1416fbd0e5a9bc4b9bd1aa
Vitor Hugo Pinto <vhpinto1@gmail.com>
2022-11-17 12:55:04 -0300

    Espaço em branco entre textos

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -21,11 +21,11 @@ export function Post() {
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉 <a href=''>jane.design/doctorcare</a></p>
+                <p>👉{' '}<a href=''>jane.design/doctorcare</a></p>
                 <p>
-                    <a href=''>#novoprojeto</a> 
-                    <a href=''>#nlw</a> 
-                    <a href=''>#rocketseat</a> 
+                    <a href=''>#novoprojeto</a>{' '}
+                    <a href=''>#nlw</a>{' '}
+                    <a href=''>#rocketseat</a>{' '}
                 </p>
             </div>
         </article>
