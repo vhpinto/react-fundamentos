c1dc97b1006c221ff6df702a8723001083466b31
Vitor Hugo Pinto <vhpinto1@gmail.com>
2022-12-19 13:01:55 -0300

    Correção da falta da tag button para o footer

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -14,16 +14,18 @@ export function Comment() {
                             <time title='11 de maio às 08:13' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
                         </div>
                         <button title='Deletar comentário'>
-                            <Trash size={20} />
+                            <Trash size={24} />
                         </button>
                     </header>
 
                     <p>Muito bom Devon, parabéns!!</p>
                 </div>
                 <footer>
-                    <ThumbsUp />
-                    Aplaudir
-                    <span>20</span>
+                    <button title='Dar like no comentário'>
+                        <ThumbsUp size={24}/>
+                        Aplaudir
+                        <span>20</span>
+                    </button>
                 </footer>
             </div>
         </div>
