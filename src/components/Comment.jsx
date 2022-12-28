81e49036f1f40396bcb3329e951890224a4173b1
Vitor Hugo Pinto <vhpinto1@gmail.com>
2022-12-28 00:20:04 -0300

    Props de content do componente Content

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import styles from './Comment.module.css';
 import { Trash, ThumbsUp } from 'phosphor-react';
 import { Avatar } from './Avatar';
 
-export function Comment() {
+export function Comment({ content }) {
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src='https://github.com/vhpinto.png' />
@@ -19,7 +19,7 @@ export function Comment() {
                         </button>
                     </header>
 
-                    <p>Muito bom Devon, parabéns!!</p>
+                    <p>{content}</p>
                 </div>
                 <footer>
                     <button title='Dar like no comentário'>
